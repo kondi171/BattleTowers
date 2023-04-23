248d2bb51c0bdd4a89e519ba2a3fc7bf9fcf53e5
Konrad Nowak <wk.k.nowak@gmail.com>
2023-04-24 01:39:32 +0200

    animate bullets

--- battle-towers-react/src/components/classes/Bullet.ts
+++ battle-towers-react/src/components/classes/Bullet.ts
@@ -1,20 +1,21 @@
 import { Position } from "../../types";
-import Enemy from "./Enemy";
+import Enemy from "./enemies/Enemy";
+import Sprite from "./Sprite";
+import bullet from './../../assets/img/sprites/projectile.png';
+import cannonBullet from './../../assets/img/towers/projectiles/cannon.png';
+import minigunBullet from './../../assets/img/towers/projectiles/minigun.png';
+import missileBullet from './../../assets/img/towers/projectiles/missile.png';
 
-class Bullet {
-    private canvasRenderingContext: CanvasRenderingContext2D;
-    private position: Position;
+class Bullet extends Sprite {
+
     private velocity: Position;
     private radius: number;
     private speed: number;
     private enemy: Enemy;
+    private bulletCanon: HTMLImageElement;
 
     constructor(ctx: CanvasRenderingContext2D, { x = 0, y = 0 }: Position, enemy: Enemy) {
-        this.canvasRenderingContext = ctx;
-        this.position = {
-            x: x,
-            y: y
-        }
+        super(ctx, { x: x, y: y }, cannonBullet);
         this.velocity = {
             x: 0,
             y: 0
@@ -22,21 +23,22 @@ class Bullet {
         this.radius = 10;
         this.speed = 10;
         this.enemy = enemy;
+        this.bulletCanon = new Image();
+        this.bulletCanon.src = cannonBullet;
     }
-
-    private draw() {
-        this.canvasRenderingContext.beginPath();
-        this.canvasRenderingContext.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
-        this.canvasRenderingContext.fillStyle = 'orange';
-        this.canvasRenderingContext.fill();
+    private draw(angle: number) {
+        this.canvasRenderingContext.save();
+        this.canvasRenderingContext.translate(this.position.x, this.position.y);
+        this.canvasRenderingContext.rotate(angle);
+        this.canvasRenderingContext.drawImage(this.bulletCanon, -this.image.width / 2, -this.image.height / 2);
+        this.canvasRenderingContext.restore();
     }
-
     public update() {
-        this.draw();
         const angle = Math.atan2(
             this.enemy.getPosition().y - this.position.y + this.enemy.getBounding().height / 2,
             this.enemy.getPosition().x - this.position.x + this.enemy.getBounding().width / 2
         );
+        this.draw(angle);
         this.velocity.x = Math.cos(angle) * this.speed;
         this.velocity.y = Math.sin(angle) * this.speed;
 
@@ -48,4 +50,5 @@ class Bullet {
     public getRadius() { return this.radius };
     public getEnemy() { return this.enemy };
 }
+
 export default Bullet;
---
+++ battle-towers-react/src/components/classes/Sprite.ts
@@ -0,0 +1,27 @@
+import { Frames, Position } from '../../types';
+
+abstract class Sprite {
+    protected canvasRenderingContext: CanvasRenderingContext2D;
+    protected position: Position;
+    protected image: HTMLImageElement;
+    protected frames: Frames;
+
+    constructor(ctx: CanvasRenderingContext2D, { x = 0, y = 0 }: Position, imageSrc: string, frames = { max: 1 }) {
+        this.canvasRenderingContext = ctx;
+        this.position = {
+            x: x,
+            y: y
+        };
+        this.image = new Image();
+        this.image.src = imageSrc;
+        this.frames = {
+            max: frames.max,
+            current: 0,
+            elapsed: 0,
+            hold: 4
+        }
+    }
+    // abstract private draw() {};
+}
+
+export default Sprite;
