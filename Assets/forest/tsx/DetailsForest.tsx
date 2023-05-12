897fb03652523bde546d6c02ed7a45ef026fd164
Konrad Nowak <wk.k.nowak@gmail.com>
2023-05-12 19:48:47 +0200

    Initial commit

--- Assets/forest/tsx/DetailsForest.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="DetailsForest" tilewidth="64" tileheight="64" tilecount="64" columns="8">
- <image source="../png/DetailsForest.png" width="512" height="512"/>
-</tileset>
