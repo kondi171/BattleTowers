edec29a90f2559b74fe08e434d42d0e0691c7328
Patryk Kaczmarski <patryk.kaczmarski98@wp.pl>
2023-06-15 02:04:43 +0200

    nuxt init

---
+++ battle-towers-nuxt/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    'jest/globals': true,
+    node: true,
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'prettier',
+  ],
+  plugins: ['jest'],
+  // add your custom rules here
+  rules: [
+    {
+      test: /\.scss$/,
+      use: [
+        'vue-style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }
+  ],
+}
---
+++ battle-towers-nuxt/components/Tutorial.vue
@@ -0,0 +1,102 @@
+<template>
+  <section class="introState">
+    <h2 class="attention">
+      <span>Attention!</span> {{ currentAttentionMessage }}<span>|</span>
+    </h2>
+    <button ref="btnRef" class="introButton" @click="handleAccept">
+      <span class="btnInfo" ref="infoRef">{{ buttonInfo }}</span>
+    </button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+}
+</script>
+<style lang="scss" scoped>
+@import './../../assets/scss/variables';
+
+.introState {
+
+  .introButton {
+    position: fixed;
+    bottom: 7%;
+    left: 25%;
+    width: 20%;
+    right: 25%;
+    display: block;
+    text-align: center;
+    margin: 0 auto;
+    height: 5vh;
+    border-radius: 4px;
+    border: 2px solid $primaryColor;
+    background-color: transparent;
+    color: $primaryColor;
+    font-size: 2rem;
+    font-weight: bold;
+    transition-duration: .4s;
+    outline: 2px solid $secondaryColor;
+    opacity: 0;
+
+    &.visible {
+      opacity: 1;
+    }
+
+    .fadeOut {
+      transition-duration: .4s;
+      opacity: 0;
+    }
+
+    .fadeIn,
+    &.fadeIn,
+    &:hover {
+      cursor: pointer;
+      transition-duration: .4s;
+      opacity: 1;
+      border-color: $primaryColor;
+      background-color: $secondaryColor;
+      color: $bgColor;
+      outline-offset: 4px;
+      outline-color: $primaryColor;
+    }
+  }
+
+  .attention {
+    position: fixed;
+    bottom: 20%;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    width: 70vw;
+    font-size: 3.3rem;
+    text-align: center;
+    line-height: 150%;
+    color: $fontColor;
+    transition-duration: .4s;
+
+    span {
+      color: $primaryColor;
+
+      &:last-of-type {
+        font-size: 4rem;
+        animation: cursor infinite .4s ease-in-out;
+      }
+
+      @keyframes cursor {
+        0% {
+          opacity: 1;
+        }
+
+        50% {
+          opacity: 0;
+        }
+
+        100% {
+          opacity: 1;
+        }
+      }
+    }
+  }
+}
+</style>
