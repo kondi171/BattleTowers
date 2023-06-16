a754ca440316d0e89914c3e8b553c644f191382b
Patryk Kaczmarski <patryk.kaczmarski98@wp.pl>
2023-06-16 02:30:30 +0200

    merge frameworks to main branch

---
+++ battle-towers-react/src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BattleTowers from './components/BattleTowers';
+import AppProvider from './components/AppContext';
+import './assets/scss/main.scss';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <AppProvider>
+      <BattleTowers />
+    </AppProvider>
+  </React.StrictMode>
+);
